cc84943226604c3542a4632248810911c196dd35
mawenhao <mawh@bxlsj.com>
2023-11-29 09:06:03 +0800

    feat: 提取字段注释

--- src/main/kotlin/cn/haloop/swi/helper/visitor/SwiGoStructVisitor.kt
+++ src/main/kotlin/cn/haloop/swi/helper/visitor/SwiGoStructVisitor.kt
@@ -1,8 +1,12 @@
 package cn.haloop.swi.helper.visitor
 
+import com.goide.psi.GoFieldDefinition
 import com.goide.psi.GoRecursiveVisitor
 import com.goide.psi.GoStructType
+import com.intellij.psi.PsiComment
+import com.intellij.psi.PsiElement
 import com.intellij.psi.ResolveState
+import com.intellij.psi.util.PsiTreeUtil
 
 /**
  * @author yangtuo
@@ -19,8 +23,9 @@ class SwiGoStructVisitor : GoRecursiveVisitor() {
                     val structMeta = StructMeta()
                     structMeta.name = fieldDef.name.toString()
                     structMeta.type = fieldDeclaration.type?.text ?: "Unknown Type"
-                    structMeta.title = fieldDef.name.toString()
-                    structMeta.desc = "" // 这里添加逻辑来提取字段描述
+                    structMeta.title =
+                        fieldDeclaration.tag?.getValue("desc") ?: fieldDeclaration.tag?.getValue("description") ?: ""
+                    structMeta.desc = findFieldComment(fieldDef)
                     structMetas.add(structMeta)
                 }
             } else {
@@ -33,19 +38,30 @@ class SwiGoStructVisitor : GoRecursiveVisitor() {
         }
     }
 
-
-    class StructMeta {
-        var name: String = ""
-        var type: String = ""
-        var title: String = ""
-        var desc: String = ""
-
-        fun toList(): MutableList<Any> {
-            return mutableListOf(name, type, title, desc)
+    private fun findFieldComment(fieldDef: GoFieldDefinition): String {
+        var nextSibling: PsiElement? = PsiTreeUtil.nextLeaf(fieldDef)
+        while (nextSibling != null) {
+            if (nextSibling is PsiComment) {
+                return nextSibling.text.trimStart('/').trim() // 提取注释文本
+            }
+            nextSibling = PsiTreeUtil.nextLeaf(nextSibling)
         }
+
+        return ""
     }
 
     fun toList(): MutableList<MutableList<Any>> {
         return structMetas.map { it.toList() }.toMutableList()
+    }
+}
+
+class StructMeta {
+    var name: String = ""
+    var type: String = ""
+    var title: String = ""
+    var desc: String = ""
+
+    fun toList(): MutableList<Any> {
+        return mutableListOf(name, type, title, desc)
     }
 }
